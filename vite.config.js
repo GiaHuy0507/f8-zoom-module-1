df99bba1be5e90ea64e9168c0c899c7cd63ae1ba
Gia Huy <nguyengiahuy141205@gmail.com>
2026-07-28 22:38:02 +0700

    phớt

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import path from "path";
+
+export default defineConfig({
+  root: "src",
+  publicDir: "../public",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    rolldownOptions: {
+      input: {
+        main: path.resolve(__dirname, "src/index.html"),
+        about: path.resolve(__dirname, "src/about.html"),
+      },
+    },
+  },
+  plugins: [ViteEjsPlugin()],
+});
